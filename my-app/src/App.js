aab126fca5c65f6ced5aa1abf35425b801b279c4
381706-1KolesovaChristina <miss-yurievna@ya.ru>
2024-05-20 16:07:36 +0300

    w1-1 (11)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
-// import './App.css';
-import Header from './Header';
-import Tech from './Tech';
+import './App.css';
+// import Header from './Header';
+// import Tech from './Tech';
 
 // function App() {
 //   return (
@@ -26,9 +26,31 @@ import Tech from './Tech';
 
 function App(){
   return(
-    <div>
-      <Header />
-      <Tech />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://spark.ru/upload/other/b_599e7149a23ec.jpg'></img>
+      </header>
+      <nav className='nav'>
+        <div>p</div>
+        <div>p</div>
+        <div>p</div>
+      </nav>
+      <div className= "content">
+        <div>
+        <img src='https://spark.ru/upload/other/b_599e7149a23ec.jpg'></img>
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          my posts
+          <div>
+            new posts
+          </div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
     </div>
   )
 }
