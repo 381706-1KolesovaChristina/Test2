4f00e30ea8dfb8d61218398a451341b2e337123d
381706-1KolesovaChristina <miss-yurievna@ya.ru>
2024-06-04 16:14:32 +0300

    04.06 (21)
    dialods add

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Kolya
+                </div>
+                <div className={s.dialog}>
+                    Nastya
+                </div>
+            </div>
+            <div className={s.massages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>how</div>
+                <div className={s.message}></div>
+            </div>
         </div>
     )
 }
