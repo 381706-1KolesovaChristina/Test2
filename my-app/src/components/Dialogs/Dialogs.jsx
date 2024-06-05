9cdd594313d10134f4533fa1d9ccd00f30339d2f
381706-1KolesovaChristina <miss-yurievna@ya.ru>
2024-06-05 16:23:38 +0300

    05/06 (23)

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { NavLink } from 'react-router-dom';
+
+
+const DialogItem = (props) => {
+    <div className={s.dialog + ' ' + s.active}>
+        <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+    </div>
+}
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
+                <DialogItem name='Dimych' id='1' />
+
                 <div className={s.dialog + ' ' + s.active}>
-                    Dimych
+                    <NavLink to={'/dialogs/1'}>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to={'/dialogs/2'}>Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to={'/dialogs/3'}>Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Kolya
+                    <NavLink to={'/dialogs/4'}>Kolya</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Nastya
+                    <NavLink to={'/dialogs/5'}>Nastya</NavLink>
                 </div>
             </div>
             <div className={s.massages}>
